dc3b23a5a533c87318337e5a9fc1d6bf94ec20f5
asardana <aman.sardana83@gmail.com>
2017-10-22 18:36:34 -0500

    Added and updated Comments

--- src/main/java/com/financial/kafka/spark/integration/SparkFinancialAnalysisStreamingMain.java
+++ src/main/java/com/financial/kafka/spark/integration/SparkFinancialAnalysisStreamingMain.java
@@ -2,7 +2,6 @@ package com.financial.kafka.spark.integration;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
@@ -10,15 +9,12 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SaveMode;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.execution.aggregate.ObjectAggregationIterator;
 import org.apache.spark.streaming.Durations;
-import org.apache.spark.streaming.api.java.JavaDStream;
 import org.apache.spark.streaming.api.java.JavaInputDStream;
 import org.apache.spark.streaming.api.java.JavaStreamingContext;
 import org.apache.spark.streaming.kafka010.ConsumerStrategies;
 import org.apache.spark.streaming.kafka010.KafkaUtils;
 import org.apache.spark.streaming.kafka010.LocationStrategies;
-import scala.collection.immutable.Stream;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
@@ -37,7 +33,7 @@ public class SparkFinancialAnalysisStreamingMain {
 
     public static void main(String args[]) {
 
-        // Read command Line Arguments for Kafka broker and Topic for publishing the Loan Records
+        // Read command Line Arguments for Kafka broker and Topic for consuming the Loan Records
         if (args != null) {
 
             kafkaBrokerEndpoint = args[0];
@@ -55,37 +51,40 @@ public class SparkFinancialAnalysisStreamingMain {
 
 
     }
+
     /**
-     * Reads the Streaming data from the file system and converts to a Dataset for running Spark SQL queries
+     * Reads the Streaming data from Kafka with a batch interval of 20 seconds and converts it into a Dataset for running Spark SQL queries
+     *
      * @param conf
      */
-    public void aggregateOnKafkaStreamingData(SparkConf conf){
+    public void aggregateOnKafkaStreamingData(SparkConf conf) {
 
-        // Creates Java Streaming Context with batch size of 10 seconds
+        // Creates Java Streaming Context with a batch size of 20 seconds
         JavaStreamingContext sparkStreamingContext = new JavaStreamingContext(conf, Durations.seconds(20));
 
+        // Creates a Discrete Stream by polling the loan data records on the Kafka Topic
         JavaInputDStream<ConsumerRecord<String, String>> loanConsumerRecordStream = KafkaUtils.createDirectStream(
                 sparkStreamingContext,
                 LocationStrategies.PreferConsistent(),
                 ConsumerStrategies.<String, String>Subscribe(Arrays.asList(loanDataIngestTopic), getKafkaConfiguration())
-                );
+        );
 
         // Counter for the non-empty batch number
-        AtomicInteger  batchNumber = new AtomicInteger(0);
+        AtomicInteger batchNumber = new AtomicInteger(0);
 
-        // Convert the streams of JavaRDD of Strings to JavaRDD of Java Beans
+        // Convert the streams of JavaRDD of ConsumerRecord to JavaRDD of Java Beans
         loanConsumerRecordStream.foreachRDD((JavaRDD<ConsumerRecord<String, String>> loanConsumerRecordRDD) -> {
 
-           // Flag to indicate if at least some records have been received and aggregated
+            // Flag to indicate if at least some records have been received and aggregated
             boolean isAggregatedDataAvailable = false;
 
-            JavaRDD<LoanDataRecord> loanDataRecordRDD =  loanConsumerRecordRDD.map((loanConsumerRecord) -> {
+            JavaRDD<LoanDataRecord> loanDataRecordRDD = loanConsumerRecordRDD.map((loanConsumerRecord) -> {
 
                 LoanDataRecord loanDataRecord = new LoanDataRecord();
 
-                String loanRecord =  loanConsumerRecord.value().toString();
+                String loanRecord = loanConsumerRecord.value().toString();
 
-                if(!(loanRecord.isEmpty() || loanRecord.contains("member_id") || loanRecord.contains("Total amount funded in policy code"))){
+                if (!(loanRecord.isEmpty() || loanRecord.contains("member_id") || loanRecord.contains("Total amount funded in policy code"))) {
 
                     // Few records have emp_title with comma separated values resulting in records getting rejected. Cleaning the data before creating Dataset
                     String updatedLine = loanRecord.replace(", ", "|").replaceAll("[a-z],", "");
@@ -101,13 +100,13 @@ public class SparkFinancialAnalysisStreamingMain {
                     loanDataRecord.setAnnualIncome(SparkFinancialAnalysisUtil.cleanRecordField(loanRecordSplits[13]));
                     loanDataRecord.setAddressState(SparkFinancialAnalysisUtil.cleanRecordField(loanRecordSplits[23]));
                     loanDataRecord.setPolicyCode(SparkFinancialAnalysisUtil.cleanRecordField(loanRecordSplits[51]));
-                }else {
+                } else {
                     System.out.println("Invalid Record line " + loanRecord);
                 }
-                
+
                 return loanDataRecord;
-                
-            }).filter(record -> record.getFundedAmt()!=null);
+
+            }).filter(record -> record.getFundedAmt() != null);
 
             // Create Spark Session
             SparkSession session = SparkSession.builder().config(conf).getOrCreate();
@@ -120,7 +119,7 @@ public class SparkFinancialAnalysisStreamingMain {
             // Is data available in the Stream, save the data in Parquet format for future use. Also, calculate the total
             // funding amount for a given state
 
-            if(!loanStatFullDataset.rdd().isEmpty()) {
+            if (!loanStatFullDataset.rdd().isEmpty()) {
 
                 loanStatFullDataset.write().mode(SaveMode.Append).parquet("/bigdata/loanStatFullDataset" + "/batch_" + batchNumber.get());
 
@@ -133,17 +132,17 @@ public class SparkFinancialAnalysisStreamingMain {
                 // Calculate the total funding amount for a given State in current RDD of streaming records
                 List<String> fundedAmountsForState = loanStatFullDataset.javaRDD().filter(row -> "IL".equalsIgnoreCase(row.getString(0))).map(row -> row.getString(2)).collect();
 
-                String totalFundedAmountForState = fundedAmountsForState.stream().reduce((x,y) -> Double.toString(Double.parseDouble(x) + Double.parseDouble(y))).get();
+                String totalFundedAmountForState = fundedAmountsForState.stream().reduce((x, y) -> Double.toString(Double.parseDouble(x) + Double.parseDouble(y))).get();
 
                 System.out.println("Total Amount funded by Lending Club in IL State in current batch : $" + new BigDecimal(totalFundedAmountForState).toPlainString());
             }
 
-            try{
+            try {
 
                 // Check if the aggregated data is available in the parquet file
-                if(isAggregatedDataAvailable) {
+                if (isAggregatedDataAvailable) {
 
-                    // Read loan data stats from Parquet Files for each micro-batch and combine them for aggregated results
+                    // Read loan data stats from Parquet Files for each micro-batch and combine them to calculate aggregated results
                     Dataset<Row> aggregatedLoanDataParquet = session.read().parquet("/bigdata/loanStatFullDataset/*");
 
                     if (!aggregatedLoanDataParquet.rdd().isEmpty()) {
@@ -158,7 +157,7 @@ public class SparkFinancialAnalysisStreamingMain {
                         System.out.println("Aggregated Total Amount funded by Lending Club in IL State : $" + new BigDecimal(totalFundedAmountForStateAgg).toPlainString());
                     }
                 }
-            }catch(Exception ex){
+            } catch (Exception ex) {
                 System.out.println("Error while processing the aggregated data");
                 ex.printStackTrace();
             }
@@ -172,7 +171,11 @@ public class SparkFinancialAnalysisStreamingMain {
         }
     }
 
-    private Map<String, Object> getKafkaConfiguration(){
+    /**
+     * Gets the Configuration for making connection to Kafka
+     * @return
+     */
+    private Map<String, Object> getKafkaConfiguration() {
 
         Map<String, Object> kafkaConfig = new HashMap();
 
